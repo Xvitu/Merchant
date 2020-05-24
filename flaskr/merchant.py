aacf38ff89931d5c3dea96998d947ddc73dd1c48
Xvitu <vas17.vs@gmail.com>
2020-05-23 22:25:11 -0300

    Remove commented code

--- flaskr/merchant.py
+++ flaskr/merchant.py
@@ -15,14 +15,3 @@ def search(search):
     response = requests.get("https://www.googleapis.com/customsearch/v1", params=params)
 
     return response.json()
-    # conn = http.client.HTTPSConnection("https://www.googleapis.com/customsearch/v1?")
-    #
-    # conn.request("GET", "/storeproduct/search/?q=coca", headers=headers)
-    #
-    # res = conn.getresponse()
-    # data = res.read()
-    #
-    # return data.decode("utf-8")
-
-
-# AIzaSyBs9BPEh4dKKatxU5YMj1_8nfWcKm7YL_g
