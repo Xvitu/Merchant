11275adf4c15787a30bd4f3bddd09cb71cc784f1
Xvitu <vas17.vs@gmail.com>
2020-07-26 19:01:01 -0300

    refactor: remove unused import

--- merchant/src/infrastructure/search/clients/gse_client.py
+++ merchant/src/infrastructure/search/clients/gse_client.py
@@ -1,9 +1,9 @@
 import requests
-import os
 from merchant.src.infrastructure.search.requests.google_search_engine_request import GoogleSearchEngineRequest
 from merchant.src.domain.entities.product import Product
 from merchant.src.infrastructure.search.responses.search_result import SearchResult
 from settings import GSE_API_URL
+
 
 class GSEClient:
     def __init__(self):
