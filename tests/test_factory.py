052c2f0fec97626d5adf2b2737047e63a34951a4
Xvitu <vas17.vs@gmail.com>
2020-07-26 18:59:43 -0300

    refactor: create a class to run test factory

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,11 +1,12 @@
 from merchant import create_app
 
 
-def test_config():
-    assert not create_app().testing
-    assert create_app({'TESTING': True}).testing
+class TestFactory:
 
+    def test_config(self):
+        assert not create_app().testing
+        assert create_app({'TESTING': True}).testing
 
-def test_health(client):
-    response = client.get('/health')
-    assert response.data == b'Got some rare things on sale, stranger!'
+    def test_health(self, client):
+        response = client.get('/health')
+        assert response.data == b'Got some rare things on sale, stranger!'
